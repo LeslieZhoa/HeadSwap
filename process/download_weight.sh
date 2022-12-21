b1bdc6c1bad28e6793c6747fe66a5025652eb290
LeslieZhoa <2289970030@qq.com>
2022-12-21 23:23:08 +0800

    up model url

--- process/download_weight.sh
+++ process/download_weight.sh
@@ -1,3 +1,3 @@
 wget https://github.com/LeslieZhoa/HeSer.Pytorch/releases/download/v0.0/parsing.pth -P ../pretrained_models
-wget https://github.com/LeslieZhoa/HeSer.Pytorch/releases/download/v0.0/sr_cf.onnx -P ../pretrained_models
-wget https://github.com/LeslieZhoa/HeSer.Pytorch/releases/download/v0.0/Blender-401-00012900.pth -P ../pretrained_models
+wget https://github.com/LeslieZhoa/HeadSwap/releases/download/v0.0/sr_cf.onnx -P ../pretrained_models
+wget https://github.com/LeslieZhoa/HeadSwap/releases/download/v0.0/Blender-401-00012900.pth -P ../pretrained_models
